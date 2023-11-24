1092e822847abe9fdf856f73dc2efb71ebd76f3f
Cristian Ojeda <cristianojeda.maimo@gmail.com>
2023-11-23 21:15:38 -0300

    fix ESLint error

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default function Page() {
         <div
         className={styles.shape}
 />
-          <h1 className="text-blue-500">I'm blue!</h1>
+          <h1 className="text-blue-500">I&apos;m blue!</h1>
           <p className={`${lusitana.className} antialiased text-xl text-gray-800 md:text-3xl md:leading-normal`}>
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
